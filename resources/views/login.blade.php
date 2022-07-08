30073267694b9c3ebdd65c3d29b9c904505dc343
Yasar Arafath <yasar.a@zucisystems.com>
2022-07-08 11:56:05 +0530

    Login Screen Start commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+@include("includes.header")
+<section>
+    <div class="row border">
+        <div class="col-md-6 offset-3">
+            <div class="form-group input-addon">
+                 <label>Username</label>
+                <input type="text">
+            </div>
+            <div class="form-group input-addon">
+                <label>Password</label>
+                <input type="text">
+             </div>
+        </div>
+    </div>
+</section>
